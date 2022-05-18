85edeff5212c82a2b315404eb3038ddc6b5db4aa
Minjae Lee <minj_shalom@mondrian.ai>
2022-05-18 19:43:00 +0900

    Modify the source code directory path

--- Documents/개인 프로젝트/serverless-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "serverlessapp": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
